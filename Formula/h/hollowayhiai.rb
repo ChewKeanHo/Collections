38128a798948f2817fcca21510bcff25c771f052
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2024-04-01 17:17:31 +0800

    automation: published as of hollowayhiai 0.0.3

--- Formula/h/hollowayhiai.rb
+++ Formula/h/hollowayhiai.rb
@@ -2,14 +2,14 @@ class Hollowayhiai < Formula
   desc "Turn every computer's terminal into an interactive AI prompt."
   homepage "https://www.hollowaykeanho.com/"
   license "Apache-2.0"
-  url "https://github.com/ChewKeanHo/Collections/releases/download/store/hollowayhiai-homebrew_0.0.2_any-any.tar.xz"
-  sha256 "a4cabf99fd97558f22761db2d28c47d02bff66e0910a1af7b7d79b80a097ce19"
+  url "https://github.com/ChewKeanHo/Collections/releases/download/store/hollowayhiai-homebrew_0.0.3_any-any.tar.xz"
+  sha256 "208bf9fb11a8f5841cd3dd5ff0c05ecfa7454d0fe47480a994f243e8a16257b0"
 
 
   def install
-    chmod 0755, "bin/hollowayHIAI.sh.ps1"
-    libexec.install "bin/hollowayHIAI.sh.ps1"
-    bin.install_symlink libexec/"hollowayHIAI.sh.ps1" => "hollowayHIAI"
+    chmod 0755, "bin/hollowayHIAI"
+    libexec.install "bin/hollowayHIAI"
+    bin.install_symlink libexec/"hollowayHIAI" => "hollowayHIAI"
   end
 
   test do
