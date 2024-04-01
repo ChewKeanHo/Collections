ba20c3a48ba3d4130e137e7913cfbddab87c0806
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2024-04-01 15:06:29 +0800

    Formula: corrected Class syntax error
    It appears Ruby requires all Classes to be Titlecase with leading
    uppercase character. Hence, let's fix it.
    
    This patch corrects Class syntax error in Formula/ directory.
    
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- Formula/h/hollowayhiai.rb
+++ Formula/h/hollowayhiai.rb
@@ -1,14 +1,15 @@
-class hollowayHIAI < Formula
+class Hollowayhiai < Formula
   desc "Turn every computer's terminal into an interactive AI prompt."
   homepage "https://www.hollowaykeanho.com/"
   license "Apache-2.0"
-  url "https://github.com/ChewKeanHo/Collections/releases/download/store/0.0.2/hollowayhiai-homebrew_0.0.2_any-any.tar.xz"
-  sha256 "65b2378bd0c65638d06bd769047ccde3520183c666ebb0736a483ec4f196a6b0"
+  url "https://github.com/ChewKeanHo/Collections/releases/download/store/hollowayhiai-homebrew_0.0.2_any-any.tar.xz"
+  sha256 "a4cabf99fd97558f22761db2d28c47d02bff66e0910a1af7b7d79b80a097ce19"
 
 
   def install
-    chmod 0755, "bin/hollowayHIAI"
-    bin.install "bin/hollowayHIAI"
+    chmod 0755, "bin/hollowayHIAI.sh.ps1"
+    libexec.install "bin/hollowayHIAI.sh.ps1"
+    bin.install_symlink libexec/"hollowayHIAI.sh.ps1" => "hollowayHIAI"
   end
 
   test do
